8588c548b110320bbc7ee4904be7effbf1237e11
JMN09 <jmn09@mail.aub.edu>
2024-07-05 09:52:22 +0300

    Added a description of the conventional commit types to the prompt

--- src/prompts.ts
+++ src/prompts.ts
@@ -108,7 +108,24 @@ const INIT_MAIN_PROMPT = (
             : ''
         }`
       : 'Do not preface the commit with anything. Conventional commit keywords:' +
-        'fix, feat, build, chore, ci, docs, style, refactor, perf, test.'
+        JSON.stringify(
+          {
+            docs: 'Documentation only changes',
+            style:
+              'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
+            refactor: 'A code change that neither fixes a bug nor adds a feature',
+            perf: 'A code change that improves performance',
+            test: 'Adding missing tests or correcting existing tests',
+            build: 'Changes that affect the build system or external dependencies',
+            ci: 'Changes to our CI configuration files and scripts',
+            chore: "Other changes that don't modify src or test files",
+            revert: 'Reverts a previous commit',
+            feat: 'A new feature',
+            fix: 'A bug fix',
+          },
+          null,
+          2
+        )
   }  
     ${
       config?.OCO_DESCRIPTION
